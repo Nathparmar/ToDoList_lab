1beaf15a5ef11346ef23f174ec14b75a77b2d954
Thomas Loughran <tloughran3@gmail.com>
2023-11-27 15:14:45 +0000

    can add to list

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -2,14 +2,15 @@ const btn = document.querySelector('#enter');
 const textInput = document.querySelector('#new-todo');
 const textOutput = document.querySelector('#text-output');
 const list = document.querySelector('#list');
+const todoForm = document.querySelector('#todo-form');
 
 textInput.addEventListener("input", (event) => {
     textOutput.innerText = event.target.value;
     console.log(textOutput);
 });
 
-btn.addEventListener("click", () => {
-
+todoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
     const newListItem = document.createElement("li");
     newListItem.innerText = textOutput.innerText;
     list.appendChild(newListItem);
